67041f37365ad4cd7971e15ef7a8ca4ad9e11ac3
Harshi-Harshith <bvlssmaniharshith@gmail.com>
2022-01-31 17:25:07 +0530

    Update Exercise2.rb

--- Exercise2.rb
+++ Exercise2.rb
@@ -1,4 +1,4 @@
 puts "Enter your input: "
-name = gets
-name = name.gsub(/a|e|i|o|u|A|E|I|O|U/,'*')
-print name
+inputstring = gets
+inputstring = inputstring.gsub(/a|e|i|o|u|A|E|I|O|U/,'*')
+print inputstring
